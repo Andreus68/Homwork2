f1349b77754b82f8036e90c491bebfdc261f286a
unknown <perecpon@yandex.ru>
2022-09-26 14:27:07 +0300

    Show

---
+++ example10/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int number = new Random().Next(100, 1000);
+int getShowTwoDigitFromNumber(int number)
+{
+    
+    int twoNumber = ((number / 10) % 10);
+    return (twoNumber);
+}
+Console.WriteLine(number);
+int result = getShowTwoDigitFromNumber(number);
+Console.WriteLine(result);
