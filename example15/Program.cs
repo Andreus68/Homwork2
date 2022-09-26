f493eab1bffd973e43573d56797cc0f03e5ffe6f
unknown <perecpon@yandex.ru>
2022-09-26 14:32:43 +0300

    Holiday

---
+++ example15/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+int Number = new Random().Next(1, 8);
+string Holiday(int Number)
+{
+    string Holiday = "";
+    if  (Number < 6)
+    {
+        Holiday = "Нет";
+    }    
+    else Holiday = "Да";
+    return Holiday;
+}
+Console.WriteLine(Number);
+string result = Holiday(Number);
+Console.WriteLine(result);
